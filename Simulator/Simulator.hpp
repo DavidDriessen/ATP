cfbdbae6a3adb2a6cccb1279906740563b9e635f
David Driessen <daviddriessen@live.nl>
2017-10-17 16:15:06 +0200

    Sim hpp

--- Simulator/Simulator.hpp
+++ Simulator/Simulator.hpp
@@ -8,26 +8,35 @@ namespace py = pybind11;
 
 #include <iostream>
 
-//py::object lemonator_proxy = py::module::import("Simulator.Lem_sim_interface.lemonator");
+#pragma GCC visibility push(hidden)
+class output_proxy {
+    py::object py_set;
+    py::object py_write;
+public:
+    output_proxy(py::object p):
+    py_set(p.attr("set")),
+    py_write(p.attr("write"))
+    {}
 
+    void set(bool i)
+    {
+        py_set(i);
+    }
 
-struct lemonator_proxy {
+    void operator << (const char* text){
+        py_write(text);
+    }
+};
+
+class lemonator_proxy {
+public:
     py::object lemonator;
-    py::object lcd;
+    output_proxy lcd;
 
-    lemonator_proxy(int com, int l, int p):lemonator(py::module::import("Simulator.Lem_sim_interface.lemonator")),
+    lemonator_proxy(int com, int l, int p):
+    lemonator(py::module::import("Simulator.Lem_sim_interface.lemonator")),
     lcd(this->lemonator.attr("lcd"))
     {
     }
 };
-
-//class lemonator_proxy {
-//public:
-//    py::object lemonator;
-//    py::object lcd;
-//
-//    lemonator_proxy(int com, int l, int p):lemonator(py::module::import("Simulator.Lem_sim_interface.lemonator")),
-//    lcd(this->lemonator.attr("lcd"))
-//    {
-//    }
-//};
+#pragma GCC visibility pop
